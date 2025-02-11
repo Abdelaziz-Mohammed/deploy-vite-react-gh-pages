ea7cb9dc22318fbf4d4d88d4b2b13bf62aa98db7
Abdelaziz Mohamed <abdelazizmo2022@gmail.com>
2025-02-11 18:46:50 +0200

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@
 function App() {
   return (
     <>
-      <a href="https://github.com/Abdelaziz-Mohammed/deploy-vite-react-gh-pages">
-        GitHub Repo
-      </a>
-      <a href="https://abdelaziz-mohammed.github.io/deploy-vite-react-gh-pages/">
-        Live Site
-      </a>
-      <a href="https://www.linkedin.com/in/abdelaziz">
-        Linkedin
-      </a>
-      <a href="https://github.com/Abdelaziz-Mohammed">
-        GitHub
-      </a>
+      <h1>How to deploy Vite React App to GitHub Pages ??!</h1>
+      <div className="links">
+        <a href="https://github.com/Abdelaziz-Mohammed/deploy-vite-react-gh-pages">
+          GitHub Repo
+        </a>
+        <a href="https://abdelaziz-mohammed.github.io/deploy-vite-react-gh-pages/">
+          Live Site
+        </a>
+        <a href="https://www.linkedin.com/in/abdelaziz">
+          Linkedin
+        </a>
+        <a href="https://github.com/Abdelaziz-Mohammed">
+          GitHub
+        </a>
+      </div>
     </>
   )
 }
